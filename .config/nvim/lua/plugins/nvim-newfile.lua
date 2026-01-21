06fefb7afaa22efa19170cc30cd7db25af6cedf8
hananitallyson <tallysonhanani@gmail.com>
2026-01-21 13:49:19 -0300

    NewFile and NewDir changes in nvim config

--- .config/nvim/lua/plugins/nvim-newfile.lua
+++ .config/nvim/lua/plugins/nvim-newfile.lua
@@ -2,51 +2,66 @@ return {
   "adibhanna/nvim-newfile.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
   keys = {
-    { "<leader>nf", "<cmd>lua _G.NewFileNoEmoji()<CR>" },
-    { "<leader>nd", "<cmd>lua _G.NewDirNoEmoji()<CR>" },
+    { "<leader>nf", "<cmd>lua NewFile()<CR>" },
+    { "<leader>nd", "<cmd>lua NewDir()<CR>" },
   },
   config = function()
     local newfile = require("nvim-newfile")
     local nui_input = require("nui.input")
+    local vim_fn = vim.fn
 
-    -- Função global para criar arquivo
-    _G.NewFileNoEmoji = function()
+    -- Configuração do plugin
+    newfile.setup({
+      languages = {
+        go = { enabled = true, package_format = "package %s", use_directory_name = true, file_extensions = { "go" } },
+        php = { enabled = true, package_format = "<?php\n\nnamespace %s;", use_directory_name = false, file_extensions = { "php" } },
+        java = { enabled = true, package_format = "package %s;", use_directory_name = false, file_extensions = { "java" } },
+        rust = { enabled = true, package_format = "// Module: %s", use_directory_name = true, file_extensions = { "rs" } },
+        python = { enabled = true, package_format = '"""Package: %s"""', use_directory_name = false, file_extensions = { "py" } },
+      },
+      project_root_patterns = { "go.mod", "composer.json", "pom.xml", "build.gradle", "build.sbt", "package.json", ".git", "Cargo.toml", "Makefile" },
+      ui = { border_style = "rounded", prompt_text = "File name: ", width = 50, height = 1 },
+      notifications = { enabled = true },
+    })
+
+    -- Função genérica para criar popup
+    local function create_popup(title, on_submit)
       local popup = nui_input({
         position = "50%",
-        size = { width = 40 },
-        border = { style = "rounded", text = { top = { " ", "New file", " "} } },
+        size = { width = 50 },
+        border = { style = "rounded", text = { top = { " ", title, " " } } },
       }, {
         prompt = "",
-        on_submit = function(value)
-          if value ~= "" then
-            newfile.create_file(value)
-          end
-        end,
+        on_submit = on_submit,
       })
-      -- fecha com ESC
       popup:map("i", "<Esc>", function() popup:unmount() end)
       popup:mount()
     end
 
-    -- Função global para criar diretório
-    _G.NewDirNoEmoji = function()
-      local popup = nui_input({
-        position = "50%",
-        size = { width = 40 },
-        border = { style = "rounded", text = { top = { " ", "New directory", " " } } },
-      }, {
-        prompt = "",
-        on_submit = function(value)
-          if value ~= "" then
-            vim.fn.mkdir(value, "p") -- cria diretórios intermediários automaticamente
-            vim.cmd("edit .")
-          end
-        end,
-      })
-      -- fecha com ESC
-      popup:map("i", "<Esc>", function() popup:unmount() end)
-      popup:mount()
+    -- Criar arquivo na pasta do buffer atual
+    _G.NewFile = function()
+      local current_dir = vim.fn.expand("%:p:h")
+      create_popup("New file", function(value)
+        if value ~= "" then
+          local old_dir = vim.fn.getcwd()
+          vim.cmd("lcd " .. current_dir)
+          newfile.create_file(value)
+          vim.cmd("lcd " .. old_dir)
+        end
+      end)
     end
+
+    _G.NewDir = function()
+      local current_dir = vim.fn.expand("%:p:h") -- pasta do arquivo atual
+      create_popup("New directory", function(value)
+        if value ~= "" then
+          local dir_path = current_dir .. "/" .. value
+          vim.fn.mkdir(dir_path, "p")  -- cria diretórios intermediários
+          vim.cmd("edit " .. dir_path)  -- abre a nova pasta
+        end
+      end)
+    end
+
   end,
 }
 

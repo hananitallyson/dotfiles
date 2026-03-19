5f60a2f63ae11533a595840067292e9c7739f766
hananitallyson <tallysonhanani@gmail.com>
2026-03-19 01:20:01 -0300

    power layout changes

--- .config/hypr/scripts/power.sh
+++ .config/hypr/scripts/power.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 selected=$(printf "⏻ Power Off\n Reboot\n󰜉 Suspend\n󰍃 Logout" \
-           | wofi --dmenu --lines 5 --prompt "Power Menu" --sort-order=alphabetical --hide-search)
+           | wofi --dmenu --lines 4 --prompt "Power Menu" --sort-order=alphabetical --hide-search --width=10%)
 
 pkill -x wofi || true
 sleep 0.1

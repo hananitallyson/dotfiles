07be6eec857c49e3bb1b2f0778781a88caa492e2
hananitallyson <tallysonhanani@gmail.com>
2026-04-24 23:56:45 -0300

    fix: oil and conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -21,3 +21,13 @@ conform.setup({
 		lsp_fallback = true,
 	},
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		local lines = vim.api.nvim_buf_get_lines(0, -2, -1, false)
+		if lines[1] ~= "" then
+			vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+		end
+	end,
+})
